8db3c2d9fb1e836fd1ca5910f9948ccfd622115e
Hasan Chowdhuri <hasanchowdhuri@hasans-macbook-air.home>
2024-01-08 03:53:55 +0100

    banner updated

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -3,17 +3,17 @@ import image from "../../Images/Image1.png";
 
 const Banner = () => {
   return (
-    <div className="grid grid-cols-2 bg-amber-600 rounded-[30px]">
+    <div className="grid grid-cols-2 bg-amber-600 rounded-[30px] md:mt-32">
       <div className="grid justify-items-end text-white">
         <div>
-          <h1 className=" md:mt-9 text-bold text-6xl">
+          <h1 className=" md:mt-9 font-bold text-6xl md:mb-5">
             Deliver Food To Your <br /> Door Step.
           </h1>
-          <h3>Authentic Food, Quick Services, Fast Delivery.</h3>
+          <h3 className="text-2xl font-bold">Authentic Food, Quick Services, Fast Delivery.</h3>
         </div>
       </div>
       <div className=" grid justify-items-start">
-        <img src={image} className="max-w-sm bg-transparent align-bottom" />
+        <img src={image} className="bg-transparent align-bottom " />
       </div>
     </div>
   );
